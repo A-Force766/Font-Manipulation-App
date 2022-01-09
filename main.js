8ed6e34d258776d66dd5dd3c64a00d1532f08823
A-Force766 <84822787+A-Force766@users.noreply.github.com>
2022-01-08 17:53:08 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+leftWristX=0;
+rightWristX=0;
+difference=0;
+function setup()
+{
+video=createCapture(VIDEO);
+video.size(550,500);
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+console.log('PoseNet Is Initialized!');
+}
+function gotPoses(results)
+{
+if(results.length>0)
+{
+console.log(results);
+leftWristX=results[0].pose.leftWrist.x;
+rightWristX=results[0].pose.rightWrist.x;
+difference=floor(leftWristX - rightWristX);
+}
+}
+function draw()
+{
+background('#FFD700');
+fill('#F90093');
+text('Max', 50, 400);
+textSize(difference);
+}
+
